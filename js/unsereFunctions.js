587407e6474084bdd568be012aac4d52cd8d7baf
hanna <hanna@hanna-X201EP>
2017-01-26 12:45:25 +0100

    Struktur der App steht
    (es fehlen Map, NFC Funktion)

--- js/unsereFunctions.js
+++ js/unsereFunctions.js
@@ -1,7 +1,7 @@
-	function setValue(valueName, value) {
-		window.localStorage.setItem(name, parseInt(value));
-	}
+function setValue(valueName, value) {
+	window.localStorage.setItem(name, parseInt(value));
+}
 
-	function getValue(valueName) {
-		return parseInt(window.localStorage.getItem(valueName));
-	}
+function getValue(valueName) {
+	return parseInt(window.localStorage.getItem(valueName));
+}
